7d89bfcb79324cc03e481dc8a98b03660fd1e027
Allyn <a740132@fmr.com>
2024-11-14 10:05:15 -0500

    Created a pipe to capitalise the first letter of the keys from the data being mapped to the table

---
+++ src/app/pipes/capilalise-first.spec.ts
@@ -0,0 +1,7 @@
+import { CapilaliseFirst } from './capilalise-first';
+
+describe('CapilaliseFirst', () => {
+  it('should create an instance', () => {
+    expect(new CapilaliseFirst()).toBeTruthy();
+  });
+});
