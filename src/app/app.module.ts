0dc338fe07d251ca796445bdd8dd7ff0814b5d21
Allyn <a740132@fmr.com>
2024-11-14 10:04:00 -0500

    Added new component and pipe to declarations array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TableComponent } from './components/table/table.component';
+import { CapitalizeFirstPipe } from './pipes/capilalise-first';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent, 
+    CapitalizeFirstPipe
   ],
   imports: [
     BrowserModule
