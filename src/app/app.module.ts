e651dd86832a5be1248f23e1a6659aefbff5019d
Dominika <a740767@fmr.com>
2024-11-14 10:46:16 -0500

    changing layout of pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,6 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FooterComponent } from './components/footer/footer.component';
-import { CapitalizeFirstPipe } from './pipes/capilalise-first';
 
 @NgModule({
   declarations: [
