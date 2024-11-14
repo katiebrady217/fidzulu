52ac6d12145d9dcb5f884418722265ec62bfd891
Dominika <a740767@fmr.com>
2024-11-14 08:50:56 -0500

    add tests to header component

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { By } from '@angular/platform-browser';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -19,5 +20,22 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a title "Fidzulu Project"', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toBe(' Fidzulu Project ');
+  })
+
+  it('should have a Home link', () => {
+    const homeLink = fixture.debugElement.query(By.css('.header-bar-link[href="/"]'));
+    expect(homeLink).toBeTruthy();
+    expect(homeLink.nativeElement.textContent).toContain('Home');
+  });
+
+  it('should have an About link', () => {
+    const aboutLink = fixture.debugElement.query(By.css('.header-bar-link[href="/about"]'));
+    expect(aboutLink).toBeTruthy();
+    expect(aboutLink.nativeElement.textContent).toContain('About');
   });
 });
