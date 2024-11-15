edc6d7f8b1fd20d756f8cdbd0a84d896ff333112
Katie Brady <a765195@fmr.com>
2024-11-15 10:13:04 -0500

    Add team names

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -6,11 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
-  teams: any[] =[{
+  teams: any[] =[
+    {
+      teamName:"Group 1",
+      members:["Allyn", "Dominika", "Katie"]
+    },
+    {
     teamName: "Group 2",
     members: ["Agata", "Alessio", "Aleks"]
-  },{
+  },
+  {
     teamName: "Group 3",
-    members: ["Agata", "Alessio"]
+    members: ["Patricijia", "Martin"]
+  },
+  {
+    teamName: "Group 4",
+    members: ["Cian", "Irene"]
+  },
+  {
+    teamName: "Group 5",
+    members: ["Bartek", "Jason", "Benji"]
+  },
+  {
+    teamName: "Group 6",
+    members: ["Ciara", "Katrina"]
+  },
+  {
+    teamName: "Group 7",
+    members: ["Thomas", "Rajat"]
   }];
+
+  
 }
